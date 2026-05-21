6ef9d8f1f2f8dc83da6bcf254fd78de8dc9c5ad8
Elynzx <rpascual06@outlook.com>
2026-05-21 18:02:52 -0500

    feat: add useTopMenuBar hook for managing top menu bar state and date display

---
+++ src/shared/components/TopMenuBar/hooks/useTopMenuBar.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { formatDate } from "../../../utils/formatDate";
+import { useWorkspaceStore } from "../../../../store/useWorkspaceStore";
+
+export const useTopMenuBar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const { isPortfolioOpen, isMusicOpen, isTasksOpen } =
+    useWorkspaceStore();
+
+  useEffect(() => {
+    const dateRefreshTimer = setInterval(
+      () => setCurrentDate(new Date()),
+      10000,
+    );
+    return () => clearInterval(dateRefreshTimer);
+  }, []);
+
+  const { timeString, dateString } = formatDate(currentDate);
+
+  const getActiveAppName = () => {
+    if (isPortfolioOpen) return "Portfolio";
+    if (isMusicOpen) return "Music Player";
+    if (isTasksOpen) return "Beary Focused App";
+
+    return "♡ My Workspace ♡";
+  };
+
+  return {
+    timeString,
+    dateString,
+    activeAppName: getActiveAppName(),
+  };
+};
