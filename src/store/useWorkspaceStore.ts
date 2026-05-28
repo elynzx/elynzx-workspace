61060129e6fcdbf253e9ff67a4dd8a84e937829d
Elynzx <rpascual06@outlook.com>
2026-05-28 16:35:35 -0500

    feat: add toggle functions and state for specs and contact in workspace store

--- src/store/useWorkspaceStore.ts
+++ src/store/useWorkspaceStore.ts
@@ -5,21 +5,30 @@ interface WorkspaceState {
   isPortfolioOpen: boolean;
   isMusicOpen: boolean;
   isTasksOpen: boolean;
+  isSpecsOpen: boolean;
+  isContactOpen: boolean;
   toggleIdCard: () => void;
   togglePortfolio: () => void;
   toggleMusic: () => void;
   toggleTasks: () => void;
+  toggleSpecs: () => void;
+  toggleContact: () => void;
 }
 
 export const useWorkspaceStore = create<WorkspaceState>((set) => ({
-  isIdCardOpen: true,
-  isPortfolioOpen: false,
+  isIdCardOpen: false,
+  isPortfolioOpen: true,
   isMusicOpen: false,
   isTasksOpen: false,
+  isSpecsOpen: false,
+  isContactOpen: false,
 
   toggleIdCard: () => set((state) => ({ isIdCardOpen: !state.isIdCardOpen })),
   togglePortfolio: () =>
     set((state) => ({ isPortfolioOpen: !state.isPortfolioOpen })),
   toggleMusic: () => set((state) => ({ isMusicOpen: !state.isMusicOpen })),
   toggleTasks: () => set((state) => ({ isTasksOpen: !state.isTasksOpen })),
+  toggleSpecs: () => set((state) => ({ isSpecsOpen: !state.isSpecsOpen })),
+  toggleContact: () =>
+    set((state) => ({ isContactOpen: !state.isContactOpen })),
 }));
