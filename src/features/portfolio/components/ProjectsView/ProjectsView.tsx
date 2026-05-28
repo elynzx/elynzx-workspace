260039b7a83cc44e908bf2a83d0a13fde11076fa
Elynzx <rpascual06@outlook.com>
2026-05-28 17:08:36 -0500

    feat: add ProjectsView component for displaying projects with ProjectCard

---
+++ src/features/portfolio/components/ProjectsView/ProjectsView.tsx
@@ -0,0 +1,13 @@
+import { PROJECTS_DATA } from "../../data/projectsData";
+import { ProjectCard } from "./components/ProjectCard";
+
+
+export const ProjectsView = () => {
+  return (
+    <div className="flex flex-col gap-24 w-full animate-in fade-in duration-300 pb-16">
+    {PROJECTS_DATA.map((project) => (
+        <ProjectCard key={project.id} {...project} />
+      ))}
+    </div>
+  );
+};
