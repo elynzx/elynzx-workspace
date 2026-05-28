d5510af983980d405a14a8676342711ec0983111
Elynzx <rpascual06@outlook.com>
2026-05-28 16:35:53 -0500

    feat: add DisplayLayout component for structured layout with background, menu, and content

---
+++ src/shared/layout/displayLayout.tsx
@@ -0,0 +1,22 @@
+import { IdCard } from "../../features/id-card/IdCard";
+import { Portfolio } from "../../features/portfolio/Portfolio";
+import { useWorkspaceStore } from "../../store/useWorkspaceStore";
+import { Background } from "../components/Background/Background";
+import { BottomBar } from "../components/BottomBar/BottomBar";
+import { TopMenuBar } from "../components/TopMenuBar/TopMenuBar";
+
+export const DisplayLayout = () => {
+  const { isIdCardOpen } = useWorkspaceStore();
+
+  return (
+    <div className="w-screen h-screen relative overflow-hidden select-none">
+      <Background />
+      <TopMenuBar />
+      <main className="absolute inset-0 top-12 bottom-20 w-full h-[calc(100vh-8rem)] p-4 overflow-hidden pointer-events-none">
+        {isIdCardOpen && <IdCard />}
+        <Portfolio />
+      </main>
+      <BottomBar />
+    </div>
+  );
+};
