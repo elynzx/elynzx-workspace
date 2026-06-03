178724bcebe5aacdac5bb966bc45de77b798c249
Elynzx <rpascual06@outlook.com>
2026-06-03 12:23:08 -0500

    feat: add PortfolioContent exports and remove unused view components

---
+++ src/features/portfolio/components/PortfolioContent/components/index.tsx
@@ -0,0 +1,4 @@
+export { Home } from "./Home/Home";
+export { Projects } from "./Projects/Projects";
+export { Skills } from "./Skills/Skills";
+export { Contact } from "./Contact/Contact";
--- src/features/portfolio/components/index.tsx
+++ src/features/portfolio/components/index.tsx
@@ -1,7 +1,3 @@
-export { HomeView } from "./HomeView/HomeView";
-export { ProjectsView } from "./ProjectsView/ProjectsView";
-export { SkillsView } from "./SkillsView/SkillsView";
-
 export { PortfolioHeader } from "./PortfolioHeader/PortfolioHeader";
 export { PortfolioSidebar } from "./PortfolioSidebar/PortfolioSidebar";
 export { PortfolioContent } from "./PortfolioContent/PortfolioContent";
