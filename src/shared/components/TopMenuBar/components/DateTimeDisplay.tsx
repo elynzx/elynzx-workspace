52339a85b0ba4de8be6f5c3df8b576a485dcb213
Elynzx <rpascual06@outlook.com>
2026-06-12 20:01:44 -0500

    fix: update styles in DateTimeDisplay component for consistency

--- src/shared/components/TopMenuBar/components/DateTimeDisplay.tsx
+++ src/shared/components/TopMenuBar/components/DateTimeDisplay.tsx
@@ -6,13 +6,13 @@ interface Props {
 }
 
 export const DateTimeDisplay = ({ date, time }: Props) => (
-  <div className="hidden md:flex bg-ui-inner/40 items-center gap-3 text-ui-text px-8 h-9 rounded-xl tabular-nums font-bold backdrop-blur-2xl transition-colors duration-300">
-    <div className="flex items-center gap-2 text-ui-text font-medium">
-      <CalendarBlankIcon size={18} weight="bold" className="text-ui-text" />
+  <div className="hidden md:flex bg-ui-inner/40 items-center gap-3 text-white px-6 h-9 rounded-xl tabular-nums font-bold backdrop-blur-2xl transition-colors duration-300">
+    <div className="flex items-center gap-2 font-medium">
+      <CalendarBlankIcon size={18} weight="bold" />
       <span>{date}</span>
     </div>
 
     <span className="opacity-40 font-light select-none">|</span>
-    <span className="tracking-wider text-ui-text">{time}</span>
+    <span className="tracking-wider ">{time}</span>
   </div>
 );
