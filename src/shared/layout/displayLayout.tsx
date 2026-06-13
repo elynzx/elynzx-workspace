cd192b2af60916174e76fb58eea1e68a3d0463be
Elynzx <rpascual06@outlook.com>
2026-06-12 20:00:28 -0500

    feat: add conditional rendering for MusicPlayer in DisplayLayout component

--- src/shared/layout/displayLayout.tsx
+++ src/shared/layout/displayLayout.tsx
@@ -1,4 +1,5 @@
 import { IdCard } from "../../features/idCard/IdCard";
+import { MusicPlayer } from "../../features/music-player/MusicPlayer/MusicPlayer";
 import { Portfolio } from "../../features/portfolio/Portfolio";
 import { useWorkspaceStore } from "../../store/useWorkspaceStore";
 import { Background } from "../components/Background/Background";
@@ -13,7 +14,10 @@ export const DisplayLayout = () => {
       <Background />
       <TopMenuBar />
       <main className="absolute inset-0 top-12 bottom-20 w-full h-[calc(100vh-8rem)] p-4 overflow-hidden pointer-events-none">
-        {isAppOpen  && <IdCard />}
+        {isAppOpen.IdCard && <IdCard />}
+        <div className={isAppOpen.MusicPlayer ? "block" : "hidden"}>
+          <MusicPlayer />
+        </div>
         <Portfolio />
       </main>
       <BottomBar />
